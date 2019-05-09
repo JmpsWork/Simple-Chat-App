4780c7bd72dac07f23dfc5101f4402f2561071b3
Jmp <46548975+JmpsWork@users.noreply.github.com>
2019-05-09 19:49:12 -0400

    initial upload

---
+++ chatapp.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+import random
+import _thread
+import socket
+from datetime import datetime
+
+
+name = 'Jmp'
+discrim = ''.join([str(random.randint(1, 9)) for _ in range(0, 4)])
+userid = ''.join([str(random.randint(1, 9)) for _ in range(0, 12)])
+print(f'{name}#{discrim} - {userid}')
+
+
+class MainApp(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.textbox = tk.Text(state=tk.DISABLED)
+        self.textbox.pack(side='top', fill=tk.BOTH, expand=True)
+        self.entrybox = tk.Entry()
+        self.entrybox.pack(fill='x', expand=True)
+        self.sendbutton = tk.Button(text='Send your message', command=self.send_text, cursor='exchange')
+        self.sendbutton.pack(side='bottom', fill='x', expand=False)
+        self.winfo_toplevel().title('Communicator')  # App name
+        self.client = None
+
+    def add_text(self, text: str, after: int=25):
+        self.textbox.config(state=tk.NORMAL)
+        if after < len(self.textbox_text.split('\n')):
+            self.textbox_removefirstline(after=after)  # This is so we can reduce the amount of lines
+        self.textbox.insert(tk.END, f'\n{text}')
+        self.textbox.update()
+        self.textbox.config(state=tk.DISABLED)
+
+    def send_text(self):
+        text = self.entrybox_text
+        now = datetime.now()
+        timestamp = f'{now.hour}:{now.minute}'
+        self.add_text(f'[{timestamp}] {name}#{discrim}: {text}')
+        self.client.send(f'[{timestamp}] {name}#{discrim}: {text}')
+        self.clear_entryboxtext()
+
+    def clear_entryboxtext(self):
+        self.entrybox.delete(0, tk.END)
+
+    def textbox_removefirstline(self, after: int=25):
+        self.textbox.config(state=tk.NORMAL)
+        text = self.textbox_text
+        if after < len(text.split('\n')):
+            end = text.find('\n')
+            text = text[end + 1:]  # + 1 to remove the newline
+            self.textbox.replace(1.0, tk.END, text)
+        self.textbox.config(state=tk.DISABLED)
+
+    @property
+    def entrybox_text(self):
+        return self.entrybox.get()
+
+    @property
+    def textbox_text(self):
+        text = self.textbox.get(1.0, tk.END)
+        return text
+
+
+class Client:
+    def __init__(self, app: MainApp):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = 6666
+        self.address = '192.168.2.25'
+        self.addr = (self.address, self.port)
+        self.id = self.connect()
+        self.app = app
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            self.client.send(f'{name},{discrim}'.encode())  # Initial payload
+            return self.client.recv(4096)
+        except Exception as _:
+            raise _
+
+    def send(self, text: str):
+        try:
+            self.client.send(text.encode())
+        except Exception as _:
+            raise _
+
+    def on_receive(self):
+        while True:
+            try:
+                data = self.client.recv(4096)
+                decoded = data.decode('utf-8')
+                if not data:
+                    break
+                print(f'Data received: {decoded}')
+                _thread.start_new_thread(self.app.add_text, (decoded,))
+            except Exception as _:
+                raise _
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry = (600, 600)
+    app = MainApp(root)
+    app.pack(side='top', fill='both', expand=True)
+
+    c = Client(app)
+    app.client = c
+    _thread.start_new_thread(c.on_receive, ())
+
+    root.mainloop()
---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+import _thread
+
+
+class Server:
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = 6666
+        self.address = '192.168.2.25'
+        self.connections = {}
+
+    def setup(self):
+        self.server.bind((self.address, self.port))
+        self.server.listen()  # Listen for client connections
+
+    def client_thread(self, conn):
+        data = conn.recv(4096)
+        decoded = data.decode()
+        print(decoded)
+        name, discrim = decoded.split(',')
+        conn.send(f'Successfully connected.'.encode())
+        conn_id = len(self.connections)
+        conn.send(f'There are {len(conn_id) - 1} users online.'.encode())
+        self.forward(f'{name}#{discrim} has joined the chat room. Say hi!', conn_id)
+        self.connections[conn_id] = conn
+        while True:
+            try:
+                data = conn.recv(4096)
+                decoded = data.decode()
+                if not decoded:
+                    break
+                print(f'Data received: {decoded}')
+                self.forward(decoded, conn_id)
+            except Exception as _:
+                print(_)
+                break
+        self.connections.pop(conn_id)
+        self.forward(f'{name}#{discrim} has left the chat room.', conn_id)
+
+    def send_to(self, send_id: int, text: str):
+        conn = self.connections[send_id]
+        conn.send(text.encode())
+
+    def forward(self, text: str, avoid_id: int):
+        encoded = text.encode()
+        for conn_id, conn in self.connections.items():
+            if conn_id != avoid_id:
+                try:
+                    conn.send(encoded)
+                except Exception as _:
+                    raise _
+
+
+print('Starting server...')
+s = Server()
+s.setup()
+print('Setup complete. Now waiting for a connection.')
+
+while True:
+    conn, address = s.server.accept()
+    print(f'Connection from {address}')
+    _thread.start_new_thread(s.client_thread, (conn,))
